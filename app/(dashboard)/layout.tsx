a7b12749bead2ec8c7f44dc8d9b79e82bf0a906b
Tunahan BUCAK <tunahanbucak1@gmail.com>
2024-09-16 17:02:48 +0300

    first commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      dashboard
+      {children}
+    </div>
+  );
+}
---
+++ app/(dashboard)/parents/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ParentsPage() {
+  return <div>ParentsPage</div>;
+}
